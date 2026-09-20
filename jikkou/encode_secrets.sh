88a262f9ff7ced50eda0f0fee0752132d59b5c8a
Florian Hussonnois <fhussonnois@kestra.io>
2026-09-20 17:29:23 +0200

    fix: fix files for jikkou and kestra

--- jikkou/encode_secrets.sh
+++ jikkou/encode_secrets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while IFS='=' read -r key value; do
-    echo "SECRET_$key=$(echo -n "$value" | base64)";
-done < .env > .secrets
+  echo "SECRET_$key=$(echo -n "$value" | base64 -w 0)"
+done < .env > .env_encoded
 
 exit 0
